8aeb06ef6269c8d781483ecae47a27e9ea6ba16a
Ruslan P <nikopolzp@gmail.com>
2022-11-18 23:43:46 +0200

    add adaptive to header

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[data-modal-open]'),
-    closeMenuBtn: document.querySelector('[data-modal-close]'),
-    menu: document.querySelector('[data-modal]'),
-    body: document.querySelector('body'),
-    // menuList: document.querySelector('.mob-menu-list'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  // refs.menuList.addEventListener('click', removeMenu);
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
   }
-  // function removeMenu() {
-  //   refs.menu.classList.add('is-hidden');
-  //   refs.body.classList.remove('no-scroll');
-  // }
 })();
