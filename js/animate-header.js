a32371fbc75c264df80cfcb990ad12526bff3654
Ruslan P <nikopolzp@gmail.com>
2022-12-28 23:58:27 +0200

    add fix header and animated bacground for it

---
+++ js/animate-header.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  let scrollPos = 100;
+  let header = document.getElementById('header');
+
+  if (document.body.scrollTop > scrollPos || document.documentElement.scrollTop > scrollPos) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+}
